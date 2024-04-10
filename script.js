291e82b7efb8bb3bc6da2783af7db61975027741
choiinju <138094962+choiinju@users.noreply.github.com>
2024-04-10 22:48:45 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const basketball = document.getElementById("basketball")
+const cook = document.getElementById("cook")
+
+
+// basketball 요소에 클릭 이벤트 리스너 추가
+basketball.addEventListener("click", function() {
+    // 사용자를 농구 영상 URL로 이동시킴
+    window.location.href = "https://www.youtube.com/watch?v=8Yd89tBEeas";
+  });
+
+
+  // cook 요소에 클릭 이벤트 리스너 추가
+  cook.addEventListener("click", function() {
+    // 사용자를 농구 영상 URL로 이동시킴
+    window.location.href = "  https://www.youtube.com/shorts/xXDfp1fNStg";
+  });
